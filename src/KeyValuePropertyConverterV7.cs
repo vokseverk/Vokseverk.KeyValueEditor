c2b8d21f9510a51b1efc8a2d69a45bdf198b35e0
Chriztian Steinmeier <chriztian@steinmeier.dk>
2021-02-24 02:15:28 +0100

    Implement PropertyValueConverter for v7

--- src/KeyValuePropertyConverterV7.cs
+++ src/KeyValuePropertyConverterV7.cs
@@ -1,26 +1,41 @@
-using Umbraco.Core.Models.PublishedContent;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Umbraco.Core;
 using Umbraco.Core.PropertyEditors;
+using Umbraco.Core.Models.PublishedContent;
 
 namespace Vokseverk {
 
-	[PropertyValueType(typeof(IEnumerable<Dictionary<string, string>>))]
+	[PropertyValueType(typeof(List<KeyValuePair<string, string>>))]
 	[PropertyValueCache(PropertyCacheValue.All, PropertyCacheLevel.Content)]
 	public class KeyValuePropertyConverter : PropertyValueConverterBase {
 
 		public override bool IsConverter(PublishedPropertyType propertyType) {
-			return propertyType.PropertyEditorAlias.Equals("Vokseverk.PropertyEditors.KeyValueEditor");
+			return propertyType.PropertyEditorAlias.Equals("Vokseverk.KeyValueEditor");
 		}
-
-		public override object ConvertSourceToObject(PublishedPropertyType propertyType, object source, bool preview) {
-			if (source == null) return IEnumerable.Empty();
-			
-			var value = new Dictionary<string, string>();
-			
-			foreach (var pair in source) {
-				value.Add(source["key"], source["value"]);
+		
+		public override object ConvertDataToSource(PublishedPropertyType propertyType, object data, bool preview) {
+			var list = new List<KeyValuePair<string, string>>();
+			if (data == null) {
+				return list;
 			}
 			
-			return value;
+			var source = data.ToString();
+			if (source.DetectIsJson()) {
+				try {
+					var json = JsonConvert.DeserializeObject<JArray>(source);
+					
+					foreach (var jt in json) {
+						var kvp = new KeyValuePair<string, string>(jt["key"].ToString(), jt["value"].ToString());
+						list.Add(kvp);
+					}
+					return list;
+				}
+				catch { /* Hmm, not JSON after all ... */ }
+			}
+			
+			return list;
 		}
 	}
 }
