cbe824fb444b4fc5eae6f003ffe45112f7a75530
Chriztian Steinmeier <chriztian@steinmeier.dk>
2022-12-29 09:50:21 +0100

    Add files for building Nuget package

--- build.sh
+++ build.sh
@@ -2,17 +2,28 @@ PKG_NAME=`grep -o ' packageAlias \"\(.*\)\"' src/package.ent | awk '{print $2}' 
 VERSION=`grep -o ' packageVersion \"\(.*\)\"' src/package.ent | awk '{print $2}' | sed 's/"//g'`
 
 # Create the dist directory if needed
-if [[ ! -d dist ]]
-	then mkdir -p dist/package
+if [[ ! -d dist/package ]]; then
+	mkdir -p dist/package
 else
 	rm dist/package/*.*
 fi
 
-# Copy files
+if [[ ! -d dist/nuget/$PKG_NAME ]]; then
+	echo "Please create the basic nuget package structure using `dotnet new umbracopackage --name $PKG_NAME` in the dist/nuget directory."
+	exit 0
+fi
+
+# Copy files to package dirs
 cp src/*.css dist/package/
 cp src/*.js dist/package/
 cp src/*.html dist/package/
 cp src/lang/*.xml dist/package/
+
+cp src/*.css dist/nuget/$PKG_NAME/App_Plugins/$PKG_NAME/
+cp src/*.js dist/nuget/$PKG_NAME/App_Plugins/$PKG_NAME/
+cp src/*.html dist/nuget/$PKG_NAME/App_Plugins/$PKG_NAME/
+cp src/lang/*.xml dist/nuget/$PKG_NAME/App_Plugins/$PKG_NAME/lang/
+
 
 # Copy the Value Converters to the dist/ folder
 cp src/*.cs dist/
@@ -24,5 +35,18 @@ xsltproc --novalid --xinclude --output dist/package/package.xml lib/packager.xsl
 xsltproc --novalid --xinclude --output dist/package/package.manifest lib/manifester.xslt src/manifest.xml
 
 
+# Create .the csproj file
+xsltproc --novalid --xinclude --output dist/nuget/$PKG_NAME/$PKG_NAME.csproj lib/packager.xslt src/csproj.xml
+
+# Create the .targets file
+xsltproc --novalid --xinclude --output dist/nuget/$PKG_NAME/buildTransitive/$PKG_NAME.targets lib/packager.xslt src/targets.xml
+
+# Create the package.manifest for the nuget package
+xsltproc --novalid --xinclude --output dist/nuget/$PKG_NAME/App_Plugins/$PKG_NAME/package.manifest lib/manifester.xslt src/manifest.xml
+
+
 # Build the ZIP file
-zip -j "dist/${PKG_NAME}-${VERSION}.zip" dist/package/* -x \*.DS_Store
+# zip -j "dist/${PKG_NAME}-${VERSION}.zip" dist/package/* -x \*.DS_Store
+
+# Build the Nuget package
+dotnet pack --output dist dist/nuget/$PKG_NAME/$PKG_NAME.csproj
