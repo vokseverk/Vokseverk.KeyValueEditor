c9e7e24834ec7f3a144e49e5bdeb0b5d87d5e7fd
Chriztian Steinmeier <chriztian@steinmeier.dk>
2022-12-31 12:52:37 +0100

    Adapt to using UMB_VERSION to build the package

--- nuget-build.sh
+++ nuget-build.sh
@@ -1,11 +1,8 @@
 PKG_NAME=`grep -o ' packageAlias \"\(.*\)\"' src/package.ent | awk '{print $2}' | sed 's/"//g'`
-VERSION9=`grep -o ' packageVersionV9 \"\(.*\)\"' src/package.ent | awk '{print $2}' | sed 's/"//g'`
-VERSION10=`grep -o ' packageVersionV10 \"\(.*\)\"' src/package.ent | awk '{print $2}' | sed 's/"//g'`
-VERSION11=`grep -o ' packageVersionV11 \"\(.*\)\"' src/package.ent | awk '{print $2}' | sed 's/"//g'`
 
-echo "UMB_VERSION: $UMB_VERSION"
+echo "Building for Umbraco $UMB_VERSION"
 
-NUGET_DIR="dist/nuget-v10/${PKG_NAME}"
+NUGET_DIR="dist/nuget-${UMB_VERSION}/${PKG_NAME}"
 PACKAGE_DIR="${NUGET_DIR}/App_Plugins/${PKG_NAME}"
 
 if [[ -d $NUGET_DIR ]]; then
@@ -27,15 +24,15 @@ cp src/lang/*.xml $PACKAGE_DIR/Lang/
 cp images/vv-keyvalueeditor-icon.png $NUGET_DIR
 
 # Copy the Value Converter and README to the nuget folder
-cp src/v10/*.cs $NUGET_DIR
-cp src/v10/*.md $NUGET_DIR
+cp src/$UMB_VERSION/*.cs $NUGET_DIR
+cp src/$UMB_VERSION/*.md $NUGET_DIR
 
 # Transform the package.xml file
 # xsltproc --novalid --xinclude --output dist/package/package.xml lib/packager.xslt src/package.xml
 
 
 # Create .the csproj file
-xsltproc --novalid --xinclude --output $NUGET_DIR/$PKG_NAME.csproj lib/packager.xslt src/v10/csproj.xml
+xsltproc --novalid --xinclude --output $NUGET_DIR/$PKG_NAME.csproj lib/packager.xslt src/$UMB_VERSION/csproj.xml
 
 # Create the .targets file
 xsltproc --novalid --xinclude --output $NUGET_DIR/buildTransitive/$PKG_NAME.targets lib/packager.xslt src/targets.xml
